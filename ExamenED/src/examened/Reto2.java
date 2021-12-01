004be0129af3437f93287c8506f9174dd7914bc3
iris <irisau2@gmail.com>
2021-12-01 17:18:26 +0100

    Acabado Reto2 del ExamenED.

--- ExamenED/src/examened/Reto2.java
+++ ExamenED/src/examened/Reto2.java
@@ -9,12 +9,15 @@ package examened;
  *
  * @author examened
  */
-public class ExamenED {
-
+public class Reto2 {
+    
     public static void main(String[] args) {
         
-        
+        for (int num = 1; num <= 1000; num++) {
+            
+            System.out.println(num);
+            
+        }
         
     }
-    
 }
